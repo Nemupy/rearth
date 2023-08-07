5b2681e2c001b83a925ce93426f65f0104f4a45c
Nemupy <82650472+Nemupy@users.noreply.github.com>
2023-08-08 00:18:45 +0900

    Add files via upload

--- rearth/__init__.py
+++ rearth/__init__.py
@@ -1,32 +1,14 @@
-import requests
-from bs4 import BeautifulSoup
+import handler
+from desc import Desc
 
 
 class Town:
-    def __init__(self, town):
-        url = "https://rearthserver.net/map/"
-        response = requests.get(url, allow_redirects=True)
-        redirected_url = response.url
-        print(redirected_url)
-        endpoint_url = f"{redirected_url}tiles/_markers_/marker_world.json"
-        response = requests.get(endpoint_url)
+    def __init__(self, town_name):
+        town_data = handler.get_data(town_name)
 
-        data = response.json()
-
-        town_data = data["sets"]["towny.markerset"]["areas"][f"{town}__0"]
-
-        desc_html = town_data["desc"]
-        desc_soup = BeautifulSoup(desc_html, "html.parser")
-        desc_dict = {
-            "title": desc_soup.find("span", style="font-size:120%").text,
-            "mayor": desc_soup.find("span", style="font-weight:bold").text,
-            "associates": [assoc.text for assoc in desc_soup.find_all("span", style="font-weight:bold")[1:]],
-            "flags": {
-                flag.text.split(":")[0].strip(): flag.text.split(":")[1].strip()
-                for flag in desc_soup.find_all("span", style="font-weight:bold")[4:]
-            }
-        }
-
+        self.fillcolor = town_data["fillcolor"]
+        self.ytop = town_data["ytop"]
+        self.color = town_data["color"]
         self.markup = town_data["markup"]
         self.x = town_data["x"]
         self.weight = town_data["weight"]
@@ -35,10 +17,16 @@ class Town:
         self.label = town_data["label"]
         self.opacity = town_data["opacity"]
         self.fillopacity = town_data["fillopacity"]
-        self.title = desc_dict["title"]
-        self.mayor = desc_dict["mayor"]
-        self.associates = desc_dict["associates"]
-        self.flags = desc_dict["flags"]
+        self.desc = Desc(town_data)
+
+    def fillcolor(self):
+        return self.fillcolor
+
+    def ytop(self):
+        return self.ytop
+
+    def color(self):
+        return self.ytop
 
     def markup(self):
         return self.markup
@@ -64,14 +52,5 @@ class Town:
     def fillopacity(self):
         return self.fillopacity
 
-    def title(self):
-        return self.title
-
-    def mayor(self):
-        return self.mayor
-
-    def associates(self):
-        return self.associates
-
-    def flags(self):
-        return self.flags
+    def desc(self):
+        return self.desc
